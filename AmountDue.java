0efb48222637da077ea59f2fbdb365b28cc94553
Kaleid <90696565+KaleidSkylark@users.noreply.github.com>
2021-10-16 20:42:22 +0800

    Update AmountDue.java

--- AmountDue.java
+++ AmountDue.java
@@ -1,16 +1,13 @@
 class AmountDue {
-
-        // Overloaded computeAmountDue(). This sum takes 1 double parameter 
+        
         public double computeAmountDue(double price) { 
             return (price)+(price*.12); 
         } 
-      
-        // Overloaded computeAmountDue(). This sum takes 2 double parameters 
+
         public double computeAmountDue(double price, double quantity) { 
             return (price)+(price*.12)*quantity; 
         } 
-      
-        // Overloaded computeAmountDue(). This sum takes 3 double parameters 
+
         public double computeAmountDue(double price, double quantity, double discount) { 
             return ((price*quantity)-discount)+(price*.12); 
         } 
